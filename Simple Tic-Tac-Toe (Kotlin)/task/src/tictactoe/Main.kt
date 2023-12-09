5053d8c6620912daa5b40daaac21d83814438f33
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-12-08 21:19:10 -0300

    Stage 1: Add basic Tic Tac Toe game functionality
    The update introduces several necessary components for a basic game of Tic Tac Toe, including a Grid interface, a Displayable interface, and their respective implementations. Also, a new 'Cell' data class has been created. Random cell population and console display capabilities have been added. The game is now initialized and started in the main function.

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -1,5 +1,61 @@
 package tictactoe
 
+data class Cell(val ch: Char) {
+    override fun toString() = ch.toString()
+}
+
+interface Grid {
+    val cells: List<MutableList<Cell>>
+}
+
+
+interface Displayable {
+    fun display(grid: Grid)
+}
+
+class SimpleGrid : Grid {
+    override var cells: List<MutableList<Cell>> = listOf(
+        mutableListOf(Cell('_'), Cell('_'), Cell('_')),
+        mutableListOf(Cell('_'), Cell('_'), Cell('_')),
+        mutableListOf(Cell('_'), Cell('_'), Cell('_'))
+    )
+
+    fun populateRandomly(){
+        val charList = mutableListOf('X', 'O')
+
+        val emptyCells = cells.withIndex().flatMap { (i, row) ->
+            row.withIndex().filter { (_, cell) -> cell.ch == '_' }.map { (j, _) -> i to j }
+        }.toMutableList()
+
+        while (emptyCells.isNotEmpty()) {                                                         ˚
+            val (i, j) = emptyCells.random()
+            emptyCells.remove(i to j)
+            val randomChar = charList[0]
+            cells[i][j] = Cell(randomChar)
+            charList.add(charList.removeAt(0))
+        }
+    }
+}
+
+class ConsoleDisplay : Displayable {
+    override fun display(grid: Grid) {
+        grid.cells.forEach(::displayRow)
+    }
+
+    private fun displayRow(row: List<Cell>) {
+        println(row.joinToString(" "))
+    }
+}
+
+class TicTacToe(private val grid: Grid, private val display: Displayable) {
+    fun start() {
+        display.display(grid)
+    }
+}
+
 fun main() {
-    // write your code here
+    val grid: Grid = SimpleGrid().apply { populateRandomly() }
+    val display: Displayable = ConsoleDisplay()
+    val game = TicTacToe(grid, display)
+    game.start()
 }
