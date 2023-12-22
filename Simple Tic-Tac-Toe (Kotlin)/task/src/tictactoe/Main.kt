4bc302aba84b1d1e15ea78718246865c64778425
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-12-22 13:47:54 -0300

    Stage 3: Refactor Grid implementation and add game state analysis
    Improved the user-defined grid initialization process by parsing the string input more efficiently. The cells in the grid are now immutable. Added functionality to analyze and print the game state, including checks for game draw, impossible state, and wins. Also, Cell class now includes pre-defined static instances of the X, O, and EMPTY cells.

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -10,10 +10,16 @@ data class Cell(val ch: Char) {
     fun isNotX() = !isX()
     fun isNotO() = !isO()
     fun isNot(ch: Char) = this.ch != ch
+
+    companion object {
+        val EMPTY: Cell = Cell('_')
+        val O: Cell = Cell('O')
+        val X: Cell = Cell('X')
+    }
 }
 
 interface Grid {
-    val cells: List<MutableList<Cell>>
+    val cells: List<List<Cell>>
 }
 
 interface Displayable {
@@ -45,20 +51,7 @@ class RandomGrid : Grid {
 }
 
 class UserDefinedGrid(input: String) : Grid {
-    override var cells: List<MutableList<Cell>> = listOf(
-        mutableListOf(Cell('_'), Cell('_'), Cell('_')),
-        mutableListOf(Cell('_'), Cell('_'), Cell('_')),
-        mutableListOf(Cell('_'), Cell('_'), Cell('_'))
-    )
-
-    init {
-        val charList = input.toList().map { Cell(it) }
-        cells = listOf(
-            charList.subList(0, 3).toMutableList(),
-            charList.subList(3, 6).toMutableList(),
-            charList.subList(6, 9).toMutableList()
-        )
-    }
+    override val cells = input.chunked(3).map { it.map { Cell(it) } }
 }
 
 class ConsoleDisplay : Displayable {
@@ -76,6 +69,52 @@ class ConsoleDisplay : Displayable {
 class TicTacToe(private val grid: Grid, private val display: Displayable) {
     fun start() {
         display.display(grid)
+        analyzeGameState(grid)
+    }
+
+    private fun analyzeGameState(grid: Grid) {
+        val isXWins = hasThreeInARow(grid, Cell.X)
+        val isOWins = hasThreeInARow(grid, Cell.O)
+        val isDraw = checkGameDraw(isXWins, isOWins)
+        val isImpossible = checkGameImpossible(isXWins, isOWins)
+
+        printGameState(isImpossible, isDraw, isXWins, isOWins)
+    }
+
+    private fun printGameState(isImpossible: Boolean, isDraw: Boolean, isXWins: Boolean, isOWins: Boolean) {
+        when {
+            isImpossible -> println("Impossible")
+            isDraw -> println("Draw")
+            isXWins -> println("X wins")
+            isOWins -> println("O wins")
+            else -> println("Game not finished")
+        }
+    }
+
+    private fun checkGameImpossible(isXWins: Boolean, isOWins: Boolean): Boolean {
+        val flattenCells = grid.cells.flatten()
+        val countDifference = Math.abs(flattenCells.count { it == Cell.X } - flattenCells.count { it == Cell.O })
+        return isXWins && isOWins || countDifference >= 2
+    }
+
+    private fun checkGameDraw(isXWins: Boolean, isOWins: Boolean): Boolean {
+        val flattenCells = grid.cells.flatten()
+        return !isXWins && !isOWins && flattenCells.none { it == Cell.EMPTY }
+    }
+
+    private fun hasThreeInARow(grid: Grid, cell: Cell): Boolean {
+        val combinations = listOf(
+            listOf(grid.cells[0][0], grid.cells[0][1], grid.cells[0][2]),
+            listOf(grid.cells[1][0], grid.cells[1][1], grid.cells[1][2]),
+            listOf(grid.cells[2][0], grid.cells[2][1], grid.cells[2][2]),
+            listOf(grid.cells[0][0], grid.cells[1][0], grid.cells[2][0]),
+            listOf(grid.cells[0][1], grid.cells[1][1], grid.cells[2][1]),
+            listOf(grid.cells[0][2], grid.cells[1][2], grid.cells[2][2]),
+            listOf(grid.cells[0][0], grid.cells[1][1], grid.cells[2][2]),
+            listOf(grid.cells[0][2], grid.cells[1][1], grid.cells[2][0])
+        )
+
+        return combinations.any { combination -> combination.all { it == cell } }
     }
 }
 
